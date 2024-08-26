edb934078b092e6e872d03634a828ef5681c95e9
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-08-26 09:48:07 -0400

    replace 3d model with text

--- src/ARScene.jsx
+++ src/ARScene.jsx
@@ -1,22 +1,25 @@
 import React, { useEffect, useRef } from 'react';
 import 'aframe';
 import '@ar-js-org/ar.js/aframe/build/aframe-ar.js';
+import { Canvas, extend } from '@react-three/fiber'
+import { OrbitControls } from "@react-three/drei";
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
+import Text from './Text.jsx'
+
+extend({ TextGeometry })
 
 function ARScene() {
   const sceneRef = useRef(null);
 
-  useEffect(() => {
-    // Any additional initialization logic can go here
-  }, []);
-
   return (
     <a-scene ref={sceneRef} embedded arjs="sourceType: webcam;">
       <a-marker preset="hiro">
-        <a-entity
+        <Text />
+        {/* <a-entity
           position="0 0 0"
           scale="0.55 0.55 0.55"
           gltf-model="/shiba/scene.gltf"
-        ></a-entity>
+        ></a-entity> */}
       </a-marker>
       <a-entity camera></a-entity>
     </a-scene>
