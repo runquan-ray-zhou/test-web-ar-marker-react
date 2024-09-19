cd6e9b8eabddeccd72a5f4c5cea0e38e0e8b9c10
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-09-19 02:14:21 -0400

    remove link tags

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -58,127 +58,126 @@ function Modal() {
             </button>
           </div>
           {happening ? (
-            <a
-              href="https://astc-web-companion-test.netlify.app/sessions/29"
-              className="#modal-container-link"
-              style={{
-                textDecoration: "none",
-                color: "black",
-              }}
-            >
-              <div className="modal-container-info-body-session">
-                <div className="modal-container-info-body-time">
-                  <p>1:50pm-2:05pm</p>
+            // <a
+            //   href="https://astc-web-companion-test.netlify.app/sessions/29"
+            //   className="#modal-container-link"
+            //   style={{
+            //     textDecoration: "none",
+            //     color: "black",
+            //   }}
+            // >
+            <div className="modal-container-info-body-session">
+              <div className="modal-container-info-body-time">
+                <p>1:50pm-2:05pm</p>
+              </div>
+              <div className="Session">
+                <div className="Session-icon">
+                  <span>
+                    <i className="fa-regular fa-calendar"></i>
+                  </span>
                 </div>
-                <div className="Session">
-                  <div className="Session-icon">
-                    <span>
-                      <i className="fa-regular fa-calendar"></i>
-                    </span>
-                  </div>
-                  <div className="Session-info">
-                    <p className="Session-info-title">
-                      Airborne Interactive: Elevating the Museum Experience with
-                      Spatial Computing:
-                    </p>
-                    <p className="Session-info-time">1:50pm-2:05pm</p>
-                    <p className="Session-info-presenters">
-                      Presenter: Shiri Burson - AYR Immersive, Chicago,
-                      Illinois, United States.
-                    </p>
-                    <p className="Session-info-location">
-                      Location: Exhibit Hall: Stage
-                    </p>
-                  </div>
-                  <div className="Session-arrow">
-                    <i className="fa-solid fa-chevron-right"></i>
-                  </div>
+                <div className="Session-info">
+                  <p className="Session-info-title">
+                    Airborne Interactive: Elevating the Museum Experience with
+                    Spatial Computing:
+                  </p>
+                  <p className="Session-info-time">1:50pm-2:05pm</p>
+                  <p className="Session-info-presenters">
+                    Presenter: Shiri Burson - AYR Immersive, Chicago, Illinois,
+                    United States.
+                  </p>
+                  <p className="Session-info-location">
+                    Location: Exhibit Hall: Stage
+                  </p>
+                </div>
+                <div className="Session-arrow">
+                  <i className="fa-solid fa-chevron-right"></i>
                 </div>
               </div>
-            </a>
+            </div>
           ) : (
+            // </a>
             <div className="modal-container-info-body-session">
-              <a
+              {/* <a
                 href="https://astc-web-companion-test.netlify.app/sessions/30"
                 className="#modal-container-link"
                 style={{
                   textDecoration: "none",
                   color: "black",
                 }}
-              >
-                <div className="modal-container-info-body-time">
-                  <p>2:10pm-2:25pm</p>
+              > */}
+              <div className="modal-container-info-body-time">
+                <p>2:10pm-2:25pm</p>
+              </div>
+              <div className="Session">
+                <div className="Session-icon">
+                  <span>
+                    <i className="fa-regular fa-calendar"></i>
+                  </span>
                 </div>
-                <div className="Session">
-                  <div className="Session-icon">
-                    <span>
-                      <i className="fa-regular fa-calendar"></i>
-                    </span>
-                  </div>
-                  <div className="Session-info">
-                    <p className="Session-info-title">
-                      Astronomy Activities for Neurodiverse Youth
-                    </p>
-                    <p className="Session-info-time">2:10pm-2:25pm</p>
-                    <p className="Session-info-presenters">
-                      Presenters:
-                      <br></br>
-                      Wendy Martin
-                      <br></br>
-                      Genevieve Ward-Wernet
-                    </p>
-                    <p className="Session-info-location">
-                      Location: Exhibit Hall: Stage
-                    </p>
-                  </div>
-                  <div className="Session-arrow">
-                    <i className="fa-solid fa-chevron-right"></i>
-                  </div>
+                <div className="Session-info">
+                  <p className="Session-info-title">
+                    Astronomy Activities for Neurodiverse Youth
+                  </p>
+                  <p className="Session-info-time">2:10pm-2:25pm</p>
+                  <p className="Session-info-presenters">
+                    Presenters:
+                    <br></br>
+                    Wendy Martin
+                    <br></br>
+                    Genevieve Ward-Wernet
+                  </p>
+                  <p className="Session-info-location">
+                    Location: Exhibit Hall: Stage
+                  </p>
                 </div>
-              </a>
-              <a
+                <div className="Session-arrow">
+                  <i className="fa-solid fa-chevron-right"></i>
+                </div>
+              </div>
+              {/* </a> */}
+              {/* <a
                 href="https://astc-web-companion-test.netlify.app/sessions/31"
                 className="#modal-container-link"
                 style={{
                   textDecoration: "none",
                   color: "black",
                 }}
-              >
-                <div className="modal-container-info-body-time">
-                  <p>2:45-3pm</p>
+              > */}
+              <div className="modal-container-info-body-time">
+                <p>2:45-3pm</p>
+              </div>
+              <div className="Session">
+                <div className="Session-icon">
+                  <span>
+                    <i className="fa-regular fa-calendar"></i>
+                  </span>
                 </div>
-                <div className="Session">
-                  <div className="Session-icon">
-                    <span>
-                      <i className="fa-regular fa-calendar"></i>
-                    </span>
-                  </div>
-                  <div className="Session-info">
-                    <p className="Session-info-title">
-                      Dinosaurs Will Always Be Awesome (¡Y Accesibles Para
-                      Todos!)
-                    </p>
-                    <p className="Session-info-time">2:45-3pm</p>
-                    <p className="Session-info-presenters">
-                      Presenter:
-                      <br></br>
-                      Jimmy Waldron, MA.Ed
-                      <br></br>
-                      Nico Vargas
-                      <br></br>
-                      Rose Maldonado
-                      <br></br>
-                      Dean R. Lomax, PhD
-                    </p>
-                    <p className="Session-info-location">
-                      Location: Exhibit Hall: Stage
-                    </p>
-                  </div>
-                  <div className="Session-arrow">
-                    <i className="fa-solid fa-chevron-right"></i>
-                  </div>
+                <div className="Session-info">
+                  <p className="Session-info-title">
+                    Dinosaurs Will Always Be Awesome (¡Y Accesibles Para Todos!)
+                  </p>
+                  <p className="Session-info-time">2:45-3pm</p>
+                  <p className="Session-info-presenters">
+                    Presenter:
+                    <br></br>
+                    Jimmy Waldron, MA.Ed
+                    <br></br>
+                    Nico Vargas
+                    <br></br>
+                    Rose Maldonado
+                    <br></br>
+                    Dean R. Lomax, PhD
+                  </p>
+                  <p className="Session-info-location">
+                    Location: Exhibit Hall: Stage
+                  </p>
                 </div>
-              </a>
+                <div className="Session-arrow">
+                  <i className="fa-solid fa-chevron-right"></i>
+                </div>
+              </div>
+              {/* </a> */}
             </div>
           )}
         </div>
